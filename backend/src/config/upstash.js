58c61fcb6e98556edac3a92ace1e3fa35f07ecd1
Feroz Hasnain <146413523+Ferozhasnain1504@users.noreply.github.com>
2025-10-18 23:05:50 +0530

    Update rate limit configuration to allow 100 requests

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// create a ratelimited that allows 100 requests per 20 minute
+// create a ratelimited that allows 100 requests per 20 seconds
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, '20 s')
+    limiter: Ratelimit.slidingWindow(100, '20 s')
 })
 
 
